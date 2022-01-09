d6807c184747f6491d2d8a2184447ad226379f48
강동구주민 <@121.167.131.12>
2022-01-09 21:56:59 +0900

    다형성, 인터페이스 등을 사용해 톰과 제리가 체력, 수분, 걷기 스텟으로 목적지까지 도달하는 프로젝트

---
+++ src/GameTest/src/textgame/Animal.java
@@ -0,0 +1,8 @@
+package GameTest.src.textgame;
+
+public interface Animal {
+	void move();
+	void drink();
+	void hp();
+	String getInformation();
+}
---
+++ src/GameTest/src/textgame/Jerry.java
@@ -0,0 +1,55 @@
+package GameTest.src.textgame;
+import java.util.Random;
+
+public class Jerry extends CharacterDTO implements Animal {
+	Random r = new Random();
+	public Jerry() {
+		super();
+		//String name, int hp, int distance, int drink
+	}
+
+	CharacterDTO chDTO = new CharacterDTO("제리",150,100,80);
+	int sumMove = chDTO.getDistance();
+	int sumDrink = chDTO.getDrink();
+	int sumHp = chDTO.getHp();
+	
+	@Override
+	public void move() {
+		int rdMove =r.nextInt(21)-20;
+		System.out.println("현재 거리 : " + sumMove);
+		System.out.println("거리가 " + rdMove + "만큼 이동되었습니다.\n");
+		sumMove = (sumMove += rdMove);
+		sumDrink -=5;
+		sumHp -= 5;
+	}
+
+	
+	@Override
+	public void drink() {
+		int rdDrink = (int)((Math.random()*(10)));
+		System.out.println("현재 수분 : " + sumDrink);
+		System.out.println("수분이 " + rdDrink + "만큼 변경되었습니다.\n");
+		sumDrink = (sumDrink += rdDrink);
+		sumHp -=5;
+	}
+
+	
+	@Override
+	public void hp() {
+		int rdHp =(int)((Math.random()*(10)));
+		System.out.println("현재 체력 : " + sumHp);
+		System.out.println("체력이 " + rdHp + "만큼 변경되었습니다.\n");
+		sumHp = (sumHp += rdHp);
+		sumDrink -= 5;
+	}
+
+	@Override
+	public String getInformation() {
+		return "현재 거리 : " + sumMove
+				+ "\n현재 수분 : " + sumDrink
+				+ "\n현재 체력 : " + sumHp
+				+ "\n";
+
+	}
+
+}
